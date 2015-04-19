0f22c1beb0372ca9b8122d58c568df54ca27ffda
Guillaume Legrain <legrainguillaume@gmail.com>
2015-04-19 15:25:43 -0700

    created server socket

--- IMU-Streamer/ViewController.m
+++ IMU-Streamer/ViewController.m
@@ -7,23 +7,59 @@
 //
 
 #import "ViewController.h"
+#import "AsyncSocket.h"
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    AsyncSocket *socket;
+    NSMutableArray *connectedSockets;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	socket = [[AsyncSocket alloc] init];
+    connectedSockets = [[NSMutableArray alloc] initWithCapacity:1];
+    [socket setDelegate:self];
+    [self initServer];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)initServer
+{
+    int port = 10001;
+    
+    NSError *error = nil;
+    if(![socket acceptOnPort:port error:&error]) {
+        NSLog(@"Error starting server: %@", error);
+        return;
+    }
+    NSLog(@"Listening on port %d ...", port);
+}
+
+- (void)onSocket:(AsyncSocket *)sock didAcceptNewSocket:(AsyncSocket *)newSocket
+{
+    [connectedSockets addObject:newSocket];
+}
+
+- (void)onSocket:(AsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port
+{
+    NSLog(@"didConnectToHost");
+    NSLog(@"%@ connected on port %d", host, port);
+    
+    NSString *message = @"Welcome :)";
+    NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
+    [sock writeData:data withTimeout:-1 tag:0];
+    //[sock readDataToData:[AsyncSocket CRLFData] withTimeout: 10 tag:0];
 }
 
 @end
