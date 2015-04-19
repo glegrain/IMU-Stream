7a3b5531a4c838f69e294ff2e80ca114d3b4084d
Guillaume Legrain <legrainguillaume@gmail.com>
2015-04-19 16:44:49 -0700

    sending heading and coordinates on socket

--- IMU-Streamer/ViewController.h
+++ IMU-Streamer/ViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *headingLabel;
 @property (weak, nonatomic) IBOutlet UILabel *statusLabel;
-
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
 @end
--- IMU-Streamer/ViewController.m
+++ IMU-Streamer/ViewController.m
@@ -14,17 +14,25 @@
 @end
 
 @implementation ViewController {
-    AsyncSocket *socket;
-    NSMutableArray *connectedSockets;
+    AsyncSocket *_socket;
+    NSMutableArray *_connectedSockets;
+    CLLocationCoordinate2D _currentCoordinate;
+    CLHeading *_currentHeading;
+    NSTimer *timer;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	socket = [[AsyncSocket alloc] init];
-    connectedSockets = [[NSMutableArray alloc] initWithCapacity:1];
-    [socket setDelegate:self];
+    
+    // init networking
     [self initServer];
+    
+    // init location services
+    [self startHeadingEvents];
+    
+    // init timer
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(sendSensorInformation) userInfo:nil repeats:YES];
     
 }
 
@@ -36,30 +44,82 @@
 
 - (void)initServer
 {
+    if (!_socket) {
+        _socket = [[AsyncSocket alloc] init];
+        [_socket setDelegate:self];
+        _connectedSockets = [[NSMutableArray alloc] initWithCapacity:1];
+    }
+    
     int port = 10001;
     
     NSError *error = nil;
-    if(![socket acceptOnPort:port error:&error]) {
+    if(![_socket acceptOnPort:port error:&error]) {
         NSLog(@"Error starting server: %@", error);
         return;
     }
     self.statusLabel.text = [NSString stringWithFormat:@"Listening on port %d ...", port];
 }
 
-- (void)onSocket:(AsyncSocket *)sock didAcceptNewSocket:(AsyncSocket *)newSocket
+- (void)onSocket:(AsyncSocket *)socket didAcceptNewSocket:(AsyncSocket *)newSocket
 {
-    [connectedSockets addObject:newSocket];
+    [_connectedSockets addObject:newSocket];
 }
 
-- (void)onSocket:(AsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port
+- (void)onSocket:(AsyncSocket *)socket didConnectToHost:(NSString *)host port:(UInt16)port
 {
-    NSLog(@"didConnectToHost");
-    NSLog(@"%@ connected on port %d", host, port);
+    self.statusLabel.text = [NSString stringWithFormat:@"%@ connected on port %d", host, port];
     
     NSString *message = @"Welcome :)";
     NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
-    [sock writeData:data withTimeout:-1 tag:0];
-    //[sock readDataToData:[AsyncSocket CRLFData] withTimeout: 10 tag:0];
+    [socket writeData:data withTimeout:-1 tag:0];
+}
+
+- (void)startHeadingEvents
+{
+    if (!self.locationManager) {
+        self.locationManager = [[CLLocationManager alloc] init];
+        [self.locationManager setDelegate:self];
+    }
+    
+    // Start location services to get the true heading.
+    self.locationManager.distanceFilter = kCLDistanceFilterNone; // All movements are reported
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [self.locationManager startUpdatingLocation];
+    
+    // Start heading updates.
+    if ([CLLocationManager headingAvailable]) {
+        self.locationManager.headingFilter = 0.1;
+        [self.locationManager startUpdatingHeading];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
+{
+    _currentHeading = newHeading;
+    self.headingLabel.text = [NSString stringWithFormat:@"%fº",newHeading.trueHeading];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation *location = locations.lastObject;
+    _currentCoordinate = location.coordinate;
+    
+    // update view
+    self.locationLabel.text = [NSString stringWithFormat:@"%fº %fº", _currentCoordinate.latitude, _currentCoordinate.longitude];
+
+}
+
+-(void)sendSensorInformation
+{
+    // send data
+    // "$accelx,accely,accelz,gyrox,gyroy,gyroz,magx,magy,magz#"
+    // '$lat,lon,heading#'
+    NSString *message = [NSString stringWithFormat:@"$%f,%f,%f#", _currentCoordinate.latitude, _currentCoordinate.longitude, _currentHeading.trueHeading];
+    NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
+    
+    for (AsyncSocket *socket in _connectedSockets) {
+        [socket writeData:data withTimeout:-1 tag:0];
+    }
 }
 
 @end
